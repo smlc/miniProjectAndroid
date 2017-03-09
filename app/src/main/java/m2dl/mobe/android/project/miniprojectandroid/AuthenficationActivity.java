08010aeff0ce99d1e08e3479a9e4a2844046cd3d
Rottana <rottanaly@ymail.com>
2017-03-09 10:53:08 +0100

    update authentification activity

--- app/src/main/java/m2dl/mobe/android/project/miniprojectandroid/AuthenficationActivity.java
+++ app/src/main/java/m2dl/mobe/android/project/miniprojectandroid/AuthenficationActivity.java
@@ -2,13 +2,28 @@ package m2dl.mobe.android.project.miniprojectandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthenficationActivity extends AppCompatActivity {
+
+    Button btnLogin, btnInscire;
+    EditText username, password;
+    EditText email, lastname, firstname, pwd1, pwd2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
+
+        btnLogin = (Button) findViewById(R.id.btn1);
+        btnInscire = (Button) findViewById(R.id.btn2);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.email);
+        lastname = (EditText) findViewById(R.id.lastname);
+        firstname = (EditText) findViewById(R.id.firstname);
     }
 }
 
--- app/src/main/java/m2dl/mobe/android/project/miniprojectandroid/MainActivity.java
+++ app/src/main/java/m2dl/mobe/android/project/miniprojectandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package m2dl.mobe.android.project.miniprojectandroid;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
